fd7fa78ab7477096c1937e852eae9c168fbc905f
cody <tcody2288@gmail.com>
2020-02-27 21:15:03 -0800

    Wrong file, updated to correct one.

--- Lesson02/calculator.js
+++ Lesson02/calculator.js
@@ -1,1 +1,12 @@
-console.log('hi');
+let x = 50;
+const y = 100;
+let a = 31;
+let b = 99;
+
+console.log(x*x);
+console.log(b%a);
+console.log(x/y);
+console.log(b-x);
+console.log(x+y);
+console.log(a%b);
+console.log(b%a);
